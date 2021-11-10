4aeb9038415a253d96dadcaa0503b766504f26c6
Daniil Demidko <demidko@farpost.com>
2021-11-10 21:20:40 +1000

    Совместимость с JDK8

--- src/main/java/com/github/demidko/bits/BitWriter.java
+++ src/main/java/com/github/demidko/bits/BitWriter.java
@@ -104,7 +104,7 @@ public class BitWriter {
     while (bytesReader.hasUnreadBits()) {
       outputStream.write(bytesReader.readByte());
     }
-    return outputStream.toString();
+    return new String(outputStream.toByteArray(), UTF_8);
   }
 
   /**
