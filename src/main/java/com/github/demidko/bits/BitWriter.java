d6f012f1fa8adcad11af927fe3e0a64e3218e4fd
Daniil Demidko <demidko@farpost.com>
2021-11-10 21:35:46 +1000

    JitPack jdk11

--- src/main/java/com/github/demidko/bits/BitWriter.java
+++ src/main/java/com/github/demidko/bits/BitWriter.java
@@ -104,7 +104,7 @@ public class BitWriter {
     while (bytesReader.hasUnreadBits()) {
       outputStream.write(bytesReader.readByte());
     }
-    return new String(outputStream.toByteArray(), UTF_8);
+    return outputStream.toString(UTF_8);
   }
 
   /**
