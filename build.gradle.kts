3804fec9f90ea38c5c7c4e2f0a15e53481fbaa9f
Daniil Demidko <demidko@farpost.com>
2021-11-10 21:15:32 +1000

    адаптация под Jitpack JDK8

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,10 +12,6 @@ dependencies {
   testImplementation("org.junit.jupiter:junit-jupiter:5.8.1")
   testImplementation("org.hamcrest:hamcrest:2.2")
 }
-java {
-  sourceCompatibility = VERSION_11
-  targetCompatibility = VERSION_11
-}
 tasks.test {
   useJUnitPlatform()
 }
